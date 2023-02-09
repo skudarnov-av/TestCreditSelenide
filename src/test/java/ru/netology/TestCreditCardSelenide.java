a8dba7fbfade3185d1f06289cbe23401ecb21be8
aleks skudarnov <skudarnovfred@mail.ru>
2023-02-10 00:23:38 +0500

    refactor test

--- src/test/java/ru/netology/TestCreditCardSelenide.java
+++ src/test/java/ru/netology/TestCreditCardSelenide.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,16 +10,17 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TestCreditCardSelenide {
     @BeforeEach
-    void shouldOpenBrowser() {open("http://localhost:9999");}
+    void shouldOpenBrowser() {
+        open("http://localhost:9999");
+    }
 
     @Test
-    void shouldSendFormIFAllCondition() {
-        SelenideElement form = $("[id=root]");
+    public void test() {
         $("[data-test-id=name] input").val("Скударнов Александр");
         $("[data-test-id=phone] input").val("+79226099999");
         $("[data-test-id=agreement]").click();
         $("[type=\"button\"]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText(("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайщее время")));
+        $("[data-test-id=order-success]").shouldHave(exactText(("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время")));
 
     }
 }
